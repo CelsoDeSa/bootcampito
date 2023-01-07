023b1a212b14fb793420cd1ab3909ed73b203e8e
Celso de Sá <celso.sa.lopes@gmail.com>
2023-01-07 02:14:11 -0300

    Fix config.host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.hosts << "bootcampito-production.up.railway.app"
+  config.hosts << "bootcampito.com"
 
   # Do not eager load code on boot.
   config.eager_load = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "bootcampito-production.up.railway.app"
+  config.hosts << "bootcampito.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
