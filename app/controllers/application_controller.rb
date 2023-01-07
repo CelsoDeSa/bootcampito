ea8e69dde0c18956db630e459bdadf69c4149bec
Celso de Sá <celso.sa.lopes@gmail.com>
2023-01-07 02:22:45 -0300

    Improve configs and adds redirect from version with www and without it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :redirect_subdomain
+
+  def redirect_subdomain
+    if request.host == 'www.booticampito.com'
+        redirect_to 'https://booticampito.com' + request.fullpath, :status => 301
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.hosts << "bootcampito.com"
+  config.hosts = [
+    "localhost",
+    "bootcampito.com",
+    "www.bootcampito.com"
+  ]
 
   # Do not eager load code on boot.
   config.eager_load = false
