5e720fbaa9a814562d047eaff4cc609b4d1bf85a
Celso de Sá <celso.sa.lopes@gmail.com>
2023-01-07 13:19:19 -0300

    Add pt/aulas and es/clases pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
 
   def index
   end
+
+  def aulas
+
+  end
+
+  def clases
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'profiles/celso'
+  get 'pt/aulas',   to: 'home#aulas'
+  get 'es/clases',  to: 'home#clases'
 
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
