578e363dde0a69bdc8fffe17962f659c1fefbc16
Celso de Sá <celso.sa.lopes@gmail.com>
2023-01-07 02:31:41 -0300

    Remove redirect_subdomain from ApplicationController (again)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :redirect_subdomain
-
-  def redirect_subdomain
-    if request.host == 'www.bootcampito.com'
-        redirect_to 'https://bootcampito.com' + request.fullpath, :status => 301
-    end
-  end
 end
