a6f1931d6091f268b610b73393ae3d44baedcea6
Celso de Sá <celso.sa.lopes@gmail.com>
2023-03-17 09:14:04 -0300

    Improve Project Conversation Starter UI, add more teasers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 ConversationStarter.create([
   { teaser: "...3 meter long snake case..."},
-  { teaser: "He said: 'sorry to poop your...'" }
+  { teaser: "He said: 'sorry to poop your...'" },
+  { teaser: "I said:'...' (ADHD kicked in) and I got the job anyway." },
+  { teaser: "50 cents saved my job (not the band)" },
+  { teaser: "When we did...for...it was a lost day for me at my previous job" },
+  { teaser: "...helps me get my job done everyday I...to do" },
+  { teaser: "Doing...helps me keep focused" },
 ])
